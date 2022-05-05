5fe46d5daa8235130e33545f46ec410fe976a24d
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2022-05-04 19:14:00 -0700

    Move submodule branch back to main.yaml

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,17 +1,9 @@
-echo "Checkout the correct comic_git_engine branch"
-ENGINE_VERSION=$(sed -n 's/Engine version = \(.*\)/\1/p' your_content/comic_info.ini)
-echo "Engine version: $ENGINE_VERSION"
-cd comic_git_engine
-git checkout "$ENGINE_VERSION"
-cd ..
-
 echo "Install dependencies"
 pip install -r comic_git_engine/scripts/requirements.txt
 python comic_git_engine/scripts/make_requirements_hooks_file.py
 pip install -r comic_git_engine/scripts/requirements_hooks.txt
 
 echo "Run python build script"
-echo "GITHUB_REPOSITORY: $1"
 export GITHUB_REPOSITORY=$1
 echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
 python comic_git_engine/scripts/build_site.py --delete-scheduled-posts
