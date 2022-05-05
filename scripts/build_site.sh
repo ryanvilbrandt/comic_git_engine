380b0cfee02d75ef31dfa3da4d381dff52da6eaf
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2022-05-04 18:00:54 -0700

    Created build_site.sh

---
+++ scripts/build_site.sh
@@ -0,0 +1,22 @@
+echo "Checkout the correct comic_git_engine branch"
+ENGINE_VERSION=$(sed -n 's/Engine version = \(.*\)/\1/p' your_content/comic_info.ini)
+echo "Engine version: $ENGINE_VERSION"
+git checkout "$ENGINE_VERSION"
+
+echo "Install dependencies"
+python -m pip install --upgrade pip
+pip install -r comic_git_engine/scripts/requirements.txt
+python comic_git_engine/scripts/make_requirements_hooks_file.py
+pip install -r comic_git_engine/scripts/requirements_hooks.txt
+
+echo "Run python build script"
+python comic_git_engine/scripts/build_site.py --delete-scheduled-posts
+
+echo "Commit files"
+git config --local user.name "Github Action"
+git config --local user.email "action@github.com"
+git add --all
+git diff-index --quiet HEAD || git commit -m "Auto-build"
+
+echo "Push changes"
+git push -f --set-upstream origin master
