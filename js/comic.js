08a386fd421ccd0cff00b03304a8a7f692c45eb2
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2022-05-07 10:35:08 -0700

    Bug fix and remove console log

--- js/comic.js
+++ js/comic.js
@@ -1,6 +1,8 @@
 export function init_overlay() {
-    console.log("foo");
-    document.querySelector("#click-for-overlay").onclick = function () { show_overlay(true); }
+    let overlay = document.querySelector("#click-for-overlay");
+    if (overlay !== null) {
+        overlay.onclick = function () { show_overlay(true); }
+    }
     document.querySelector("#comic-page-overlay").onclick = function () { show_overlay(false); }
 }
 
