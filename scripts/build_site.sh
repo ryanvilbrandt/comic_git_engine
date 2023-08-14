ba4f3a5f3c091976a099cc84fed73a19de12ce1e
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2023-08-14 11:53:07 -0700

    Return error if Python script fails

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -6,10 +6,10 @@ pip install -r comic_git_engine/scripts/requirements_hooks.txt
 echo "Run python build script"
 export GITHUB_REPOSITORY=$1
 echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
-python comic_git_engine/scripts/build_site.py --delete-scheduled-posts
-echo "python script return code: $?"
-if [ $? -ne 0 ]; then
-  exit $?
+export python_exit_code=python comic_git_engine/scripts/build_site.py --delete-scheduled-posts
+echo "python script exit code: $python_exit_code"
+if [ $python_exit_code -ne 0 ]; then
+  exit $python_exit_code
 fi
 
 echo "Commit files"
