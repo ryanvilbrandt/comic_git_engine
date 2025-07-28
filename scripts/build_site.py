dd6f94d677c45fd7734ff510d65094f1038c1785
Github Action <action@github.com>
2025-07-28 16:54:32 +0000

    Version 0.4.4

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -23,7 +23,7 @@ import utils
 from build_rss_feed import build_rss_feed
 from utils import read_info
 
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 
 BASE_DIRECTORY = ""
 MARKDOWN = Markdown(extras=["strike", "break-on-newline", "markdown-in-html"])
